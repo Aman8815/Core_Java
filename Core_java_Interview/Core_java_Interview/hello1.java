9a937e8e7593dd6e2be86939dd5e895e0fcb7796
Aman8815 <amanyashona465@gmail.com>
2025-09-09 15:10:02 +0530

    done

--- Core_java_Interview/Core_java_Interview/hello1.java
+++
@@ -1,6 +0,0 @@
-public class hello1 {
-    public static void main(String[] args) {
-        System.out.println("Hello Aman Singh Yashona");
-        System.out.println("hello Deepk Radhore");
-    }
-}
